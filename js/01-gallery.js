ba8bf73eac9da31b99b9e144ecf238edd8652830
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-11-11 18:46:19 +0200

    solution task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const mainDivContainerRef = document.querySelector(".gallery");
+
+function createGalleryCardEl(galleres) {
+  return galleres
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"/>
+            </a>
+           </div> `;
+    })
+    .join("");
+}
+const galleryCard = createGalleryCardEl(galleryItems);
+mainDivContainerRef.insertAdjacentHTML("beforeend", galleryCard);
+
+mainDivContainerRef.addEventListener("click", (e) => {
+  const imgSrcOriginal = e.target.dataset.source;
+  if (e.target.nodeName === "A" || imgSrcOriginal === imgSrcOriginal) {
+    e.preventDefault();
+  }
+  if (e.target === mainDivContainerRef) {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${imgSrcOriginal}">
+	`
+    )
+    .show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+new SimpleLightbox(".some-element a", {
+  /* options */
+});
+
+const mainContainerGalleryRef = document.querySelector(".gallery");
+
+function createGalleryCardEl(galleres) {
+  return galleres
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__item" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a> `;
+    })
+    .join("");
+}
+
+const galleryCard = createGalleryCardEl(galleryItems);
+mainContainerGalleryRef.insertAdjacentHTML("beforeend", galleryCard);
